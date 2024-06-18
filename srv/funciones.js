1e08eab909910d206c3d875c682b7d4e3de9efba
sebastian vidal <svidal@seidor.com>
2024-06-18 13:54:57 +0000

    svl: generar log

--- srv/funciones.js
+++ srv/funciones.js
@@ -53,73 +53,75 @@ function _formatTime(date_time){
 async function _insertSolicitud(objSolicitud){
 
   try {
-    let objNew  = {
-      "IDDCTOBTP":  uuid() ,
-      "FK_ZSD_TB_SECUENC_IDSECUENCIA": null,
-      "FK_ZSD_TB_REGLAS_IDREGLA": null,
-      "CODEJECUTIVO": objSolicitud.CodEjecutivo,
-      "DESCEJECUTIVO": objSolicitud.DescEjecutivo,
-      "PEDIDO": objSolicitud.Pedido,
-      "POSPEDIDO": objSolicitud.PosPedido,
-      "CODLOCAL": objSolicitud.CodLocal,
-      "DESCLOCAL": objSolicitud.DescLocal,
-      "CODPREVENTA": objSolicitud.CodPreventa,
-      "DESCPREVENTA": objSolicitud.DescPreventa,
-      "CODVENDEDOR": objSolicitud.CodVendedor,
-      "DESCVENDEDOR":objSolicitud.DescVendedor,
-      "CODCLIENTE": objSolicitud.CodCliente,
-      "DESCCLIENTE": objSolicitud.DescCliente,
-      "CODCENTRO": objSolicitud.CodCentro,
-      "CODMATERIAL": objSolicitud.CodMaterial,
-      "DESCMATERIAL": objSolicitud.DescMaterial,
-      "CODSECTOR": objSolicitud.CodSector,
-      "DESCSECTOR": objSolicitud.DescSector,
-      "CODESTADO": objSolicitud.CodEstado,
-      "DESCESTADO": objSolicitud.DescEstado,
-      "CODCLASIFICACION": objSolicitud.CodClasficacion,
-      "DESCCLASIFICACION": objSolicitud.DescClasificacion,
-      "PRECIOLISTAUNI": objSolicitud.PrecioListaUni,
-      "VALORRECARGOUNI": objSolicitud.ValorRecargoUni,
-      "PRECIOWFCONDSCTOUNI": objSolicitud.PrecioWFConDsctoUni,
-      "CANTIDAD":objSolicitud.Cantidad,
-      "UMV": objSolicitud.Umv,
-      "UMP": objSolicitud.Ump,
-      "PORCENTAJEDSCTO":objSolicitud.PorcentajeDscto,
-      "FECHADESPACHO":objSolicitud.FechaDespacho,
-      "CODMOTIVO": objSolicitud.CodMotivo,
-      "DESCMOTIVO": objSolicitud.DescMotivo,
-      "ESTATUS": objSolicitud.Estatus,
-      "CODUSERAPROB": objSolicitud.CodUserAprob,
-      "DESCUSERAPROB": objSolicitud.DescUserAprob,
-      "FECHARECEPSOL": objSolicitud.FechaRecepSol,
-      "HORARECEPSOL": objSolicitud.HoraRecepSol,
-      "FECHAAPROBSOL": null,
-      "HORAAPROBSOL": null,
-      "CODSUCURSALLOC": objSolicitud.CodSucursalLoc,
-      "DESCSUCURSALLOC": objSolicitud.DescSucursalLoc,
-      "CODTIPOCLIENTE": objSolicitud.CodTipocCiente,
-      "DESCTIPOCLIENTE": objSolicitud.DescTipoCliente,
-      "CODSBTPCLIENTE": objSolicitud.CodSbtpCliente,
-      "DESCSBTPCLIENTE": objSolicitud.DescSbtpCliente,
-      "CODGRCONDCLIENTE": objSolicitud.CodGrcondCliente,
-      "DESCGRCONDCLIENTE": objSolicitud.DescGrcondCliente,
-      "TIPOPROCES": null,
-      "CODMOTDERRECH": null,
-      "MOTDERRECH": null,
-      "PRECIOREGLA": null,
-      "CANTREGLA": null,
-      "PROCENTREGLA": null,
-      "FECHARECEPSOL_AUX": null,
-      "HORARECEPSOL_AUX": null,
-      }
+    let objNew = {};
+
+    console.log(objSolicitud);
+    objNew.IDDCTOBTP=  uuid() ;
+    objNew.FK_ZSD_TB_SECUENC_IDSECUENCIA= null;
+    objNew.FK_ZSD_TB_REGLAS_IDREGLA= null;
+    objNew.CODEJECUTIVO= objSolicitud.CodEjecutivo;
+    objNew.DESCEJECUTIVO= objSolicitud.DescEjecutivo;
+    objNew.PEDIDO= objSolicitud.Pedido;
+    objNew.POSPEDIDO= objSolicitud.PosPedido;
+    objNew.CODLOCAL= objSolicitud.CodLocal;
+    objNew.DESCLOCAL= objSolicitud.DescLocal;
+    objNew.CODPREVENTA= objSolicitud.CodPreventa;
+    objNew.DESCPREVENTA= objSolicitud.DescPreventa;
+    objNew.CODVENDEDOR= objSolicitud.CodVendedor;
+    objNew.DESCVENDEDOR=objSolicitud.DescVendedor;
+    objNew.CODCLIENTE= objSolicitud.CodCliente;
+    objNew.DESCCLIENTE= objSolicitud.DescCliente;
+    objNew.CODCENTRO= objSolicitud.CodCentro;
+    objNew.CODMATERIAL= objSolicitud.CodMaterial;
+    objNew.DESCMATERIAL= objSolicitud.DescMaterial;
+    objNew.CODSECTOR= objSolicitud.CodSector;
+    objNew.DESCSECTOR= objSolicitud.DescSector;
+    objNew.CODESTADO= objSolicitud.CodEstado;
+    objNew.DESCESTADO= objSolicitud.DescEstado;
+    objNew.CODCLASIFICACION= objSolicitud.CodClasficacion;
+    objNew.DESCCLASIFICACION= objSolicitud.DescClasificacion;
+    objNew.PRECIOLISTAUNI= objSolicitud.PrecioListaUni;
+    objNew.VALORRECARGOUNI= objSolicitud.ValorRecargoUni;
+    objNew.PRECIOWFCONDSCTOUNI= objSolicitud.PrecioWFConDsctoUni;
+    objNew.CANTIDAD=objSolicitud.Cantidad;
+    objNew.UMV= objSolicitud.Umv;
+    objNew.UMP= objSolicitud.Ump;
+    objNew.PORCENTAJEDSCTO=objSolicitud.PorcentajeDscto;
+    objNew.FECHADESPACHO=objSolicitud.FechaDespacho.substring(0,10);
+    objNew.CODMOTIVO= objSolicitud.CodMotivo;
+    objNew.DESCMOTIVO= objSolicitud.DescMotivo;
+    objNew.ESTATUS= objSolicitud.Estatus;
+    objNew.CODUSERAPROB= objSolicitud.CodUserAprob;
+    objNew.DESCUSERAPROB= objSolicitud.DescUserAprob;
+    objNew.FECHARECEPSOL= objSolicitud.FechaRecepSol.substring(0,10);
+    objNew.HORARECEPSOL= objSolicitud.HoraRecepSol;
+    objNew.FECHAAPROBSOL= null;
+    objNew.HORAAPROBSOL= null;
+    objNew.CODSUCURSALLOC= objSolicitud.CodSucursalLoc;
+    objNew.DESCSUCURSALLOC= objSolicitud.DescSucursalLoc;
+    objNew.CODTIPOCLIENTE= objSolicitud.CodTipocCiente;
+    objNew.DESCTIPOCLIENTE= objSolicitud.DescTipoCliente;
+    objNew.CODSBTPCLIENTE= objSolicitud.CodSbtpCliente;
+    objNew.DESCSBTPCLIENTE= objSolicitud.DescSbtpCliente;
+    objNew.CODGRCONDCLIENTE= objSolicitud.CodGrcondCliente;
+    objNew.DESCGRCONDCLIENTE= objSolicitud.DescGrcondCliente;
+    objNew.TIPOPROCES= null;
+    objNew.CODMOTDERRECH= null;
+    objNew.MOTDERRECH= null;
+    objNew.PRECIOREGLA= null;
+    objNew.CANTREGLA= null;
+    objNew.PROCENTREGLA= null;
     
       const backendConnectionWP = await cds.connect.to('Workflow_Precios')
       const { ZSD_TB_SOL_MAT_DCTO_BTP } = backendConnectionWP.entities
     
-      await backendConnectionWP.run(INSERT(objNew).into(ZSD_TB_SOL_MAT_DCTO_BTP))
+      let resultado = await backendConnectionWP.run(INSERT(objNew).into(ZSD_TB_SOL_MAT_DCTO_BTP))
+     
+      console.log(`WP - ReplicarSolicitudes:  Solicitud ${resultado}`)
+
     
   } catch (error) {
-    console.log(`WP - ReplicarSolicitudes: Error Solicitud: ${objSolicitud.Pedido} ${objSolicitud.PosPedido}: Error ${error}`)
+    console.log(`WP - ReplicarSolicitudes:  Error ${error}`)
   }
  
 
@@ -153,9 +155,11 @@ async function Replicar(req) {
   const backendConnection = await cds.connect.to('ODATA_ECC1')
   const { SolicitudesSet } = backendConnection.entities
 
-  let sQuery = `FechaRecepSol = '${sDate}' and HoraRecepSol = '${sTime}' and  Estatus = 'P'`;
+  let sQuery = `FechaRecepSol = '${sDate}' and HoraRecepSol >= '${sTime}' and  Estatus = 'P'`;
   let queryResult = await backendConnection.run(SELECT(SolicitudesSet).where(sQuery))
 
+  console.log("Query ejecutada: " + sQuery);
+  console.log("Registros encontrados: " + queryResult.length);
   if(queryResult.length > 0){
 
     const backendConnectionWP = await cds.connect.to('Workflow_Precios')
@@ -165,25 +169,26 @@ async function Replicar(req) {
 
       let element = queryResult[ele]
 
-      //Validar si el registro existe en la tabla de solicitudes
-  
+
+     // console.log(element);
       const existeSol = await backendConnectionWP.run(SELECT(ZSD_TB_SOL_MAT_DCTO_BTP).
       where(`PEDIDO = '${element.Pedido}' and POSPEDIDO = '${element.PosPedido}'`))
   
-      if(existeSol){
-        _insertSolicitud.apply(element);
+
+      if(existeSol.length == 0){
+        _insertSolicitud(element);
+       // console.log(`WP - ReplicarSolicitudes: Registros ${queryResult.length} procesados`)
       }
   
     }
 
- 
-    console.log(`WP - ReplicarSolicitudes: Registros ${queryResult.length} procesados`)
-    return `Registros ${queryResult.length} procesados`;
   }else{
 
     console.log("WP - ReplicarSolicitudes: No hay registros para procesar")
   }
-  
+
+  return JSON.stringify(queryResult)
+
 
 }
 
